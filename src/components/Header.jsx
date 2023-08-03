8ad7f8c9674e44c4bc2345958e46658fcc734d05
MATHEUS BECKER <matheus_becker@estudante.sesisenai.org.br>
2023-08-03 09:50:53 -0300

    inicio Nav-Link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,18 @@
 export default function Header(){
     return(
-        <div className="h-1/6 bg-blue-500">
-
+        <div className="h-1/6 bg-gradient-to-r from-blue-500 to-blue-600 flex flex-row items-center justify-center">
+                <div className="w-11/12 flex justify-around">
+                    <h1>LOGO</h1>
+                    <div className="flex justify-around w-2/6">
+                        <p>Home</p>
+                        <p>Product</p>
+                        <p>Pricing</p>
+                        <p>Contact</p>
+                    </div>
+                    <div>
+                        
+                    </div>
+                </div>
         </div>
     )
 }
